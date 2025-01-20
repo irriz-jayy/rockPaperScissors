e1fd11d0ac69a016644d0e2c1b2a3cedb2711f2e
irriz-jayy <jayfelix789@gmail.com>
2025-01-20 12:48:05 +0300

    added a function to randomise the computers choice

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-console.log("I am Jay Felix");
+function getComputerChoice() {
+  let a = Math.floor(Math.random() * 3) + 1;
+
+  let compChoice;
+  if (a === 1) {
+    compChoice = "rock";
+  } else if (a === 2) {
+    compChoice = "paper";
+  } else {
+    compChoice = "scissors";
+  }
+
+  return compChoice;
+}
+
+console.log(getComputerChoice());
